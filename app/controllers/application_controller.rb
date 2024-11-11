04c47fca296ed320cd86d61b51106845208e2bac
Lucas Tosetto Morvillo <lucas.tosetto@cloudwalk.io>
2024-11-11 10:27:54 -0300

    chore: added common methods for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  protected
+
+  def render_unprocessable_entity(exception)
+    errors = exception.respond_to?(:errors) ? exception.errors : [exception.message]
+    render json: { errors: errors }, status: :unprocessable_entity
+  end
+
+  def render_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
 end
